b18fb4470843cd574426e29235b469d7efe96bc4
Edwin Paredes <paredes_844@hotmail.com>
2026-05-11 15:59:03 -0500

    Ingles

--- index.js
+++ index.js
@@ -70,7 +70,7 @@
 
  
 window.onload = function () {
-  document.getElementById('modal-banner-4').style.display = "block";
+  document.getElementById('modal-banner-1').style.display = "block";
 };
 
 // Función para cerrar el primer modal y abrir el segundo
@@ -90,9 +90,9 @@ function closeModal4() {
  //document.getElementById('modal-banner-5').style.display = 'block';
 }
 
-function closeModal10() {
-  document.getElementById('modal-banner-6').style.display = 'none';
- //document.getElementById('modal-banner-4').style.display = 'block';
+function closeModal1() {
+  document.getElementById('modal-banner-1').style.display = 'none';
+ document.getElementById('modal-banner-4').style.display = 'block';
 }
 
 function closeModal5() {
